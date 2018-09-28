93d611ab21114f346d7ca08c45f8b6df9e15f71c
Olivier Breuleux <breuleux@gmail.com>
2018-09-28 18:33:14 -0400

    Add grad tests

--- tests/test_grad.py
+++ tests/test_grad.py
@@ -1,9 +1,17 @@
 
+import pytest
+import numpy as np
+
+from myia.api import standard_debug_pipeline
 from myia.composite import grad
+from myia.debug.finite_diff import gen_variants, GradTester
 from myia.dtype import JTagged as JT, SensitivityMap as SM
 from myia.infer import InferenceError
+from myia.prim import ops as P
 from myia.prim.py_implementations import scalar_cast, J, Jinv, \
-    array_reduce, scalar_add
+    array_reduce, scalar_add, typeof
+from myia.prim.grad_implementations import augmented_graphs
+from myia.prim.py_implementations import py_implementations as pyi
 
 from .common import B, T, L, F, i16, i32, i64, u64, f16, f32, f64, \
     li32, li64, lf64, ai16, ai32, ai64, af16, af32, af64, Nil, \
@@ -94,3 +102,87 @@ def test_grad_reduce(xs, ys):
         return array_reduce(scalar_add, xs * ys, ())
 
     return grad(f)(xs, ys)
+
+
+def test_GradTester():
+
+    def f(x, y):
+        return x / y
+
+    def df(x, y, dz):
+        return dz / y, -dz * x / (y * y)
+
+    arglist = (
+        (7.3, 4.2),
+        (np.array([[4.3, 2.0], [5.1, 7.7], [3.4, 8.2]]),
+         np.array([[1.2, 5.0], [3.3, 2.7], [6.9, 7.2]])),
+    )
+
+    for args in arglist:
+        gtest = GradTester(
+            fn=f,
+            gfn=df,
+            args=args,
+            argnames=['x', 'y'],
+            outnames=['out']
+        )
+
+        gtest.assert_match()
+
+
+def test_GradTester_outtup():
+
+    def f(x, y):
+        return x * y, x / y
+
+    def df(x, y, dz):
+        dz1, dz2 = dz
+        return (dz1 * y + dz2 / y,
+                dz1 * x + -dz2 * x / (y * y))
+
+    gtest = GradTester(
+        fn=f,
+        gfn=df,
+        args=(7.3, 4.2),
+        argnames=['x', 'y'],
+        outnames=None
+    )
+
+    gtest.assert_match()
+
+
+prim_tests = {
+    P.scalar_add: [(-7.1, 4.3)],
+    P.scalar_sub: [(-7.1, 4.3)],
+    P.scalar_mul: [(-7.1, 4.3)],
+    P.scalar_div: [(-7.1, 4.3)],
+    P.scalar_pow: [(7.1, 4.3), (5.3, -1.2)],
+    P.scalar_uadd: [(-7.1,)],
+    P.scalar_usub: [(-7.1,)],
+    # P.scalar_gt: [(-7.1, 4.3)],
+    # P.scalar_lt: [(-7.1, 4.3)],
+    # P.scalar_ge: [(-7.1, 4.3)],
+    # P.scalar_le: [(-7.1, 4.3)],
+}
+
+
+@pytest.mark.parametrize('prim,cases', prim_tests.items())
+def test_prim_grads(prim, cases):
+    primg = augmented_graphs[prim]
+    g = grad.make_gf(primg, primg.parameters,
+                     dbg=primg.debug, sens_param=True, get_all=True)
+    pip = standard_debug_pipeline \
+        .select('resolve', 'infer', 'specialize', 'opt', 'export') \
+        .make()
+    types = [{'type': typeof(arg)} for arg in cases[0]]
+    res = pip(graph=g, argspec=[*types, types[0]])
+
+    gtest = GradTester(
+        fn=pyi[prim],
+        gfn=res['output'],
+        args=cases[0],
+        argnames=[None]*len(cases[0]),
+        outnames=None
+    )
+
+    gtest.assert_match()
