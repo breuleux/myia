58585cb07b62bd4f1d746a1c70da8bb3a5a8cfa9
Olivier Breuleux <breuleux@gmail.com>
2018-10-23 17:19:12 -0400

    Nice label for SymbolicKeyInstance

--- myia/debug/label.py
+++ myia/debug/label.py
@@ -1,7 +1,7 @@
 """Utilities to generate or map labels for nodes and graphs."""
 
 
-from ..dtype import EnvInstance
+from ..dtype import EnvInstance, SymbolicKeyInstance
 from ..info import DebugInfo
 from ..ir.anf import ANFNode, Graph
 from ..prim import Primitive
@@ -112,6 +112,8 @@ class NodeLabeler:
                 return v.name
             elif isinstance(v, CosmeticPrimitive):
                 return v.label
+            elif isinstance(v, SymbolicKeyInstance):
+                return f'{self.name(v.node)}::Node'
             elif isinstance(v, EnvInstance):
                 if len(v):
                     return 'EnvType(...)'
